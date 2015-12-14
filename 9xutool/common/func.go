c805f3d9c676d59ae43d0e8ba520b8f3c2fe2f99
zhengzhe <zhengzhe@pv25.com>
2015-12-14 13:40:11 +0800

    完成标签对应次数的小程序

--- 9xutool/common/func.go
+++ 9xutool/common/func.go
@@ -1,9 +1,11 @@
 package common
 
 import (
+	"github.com/qgweb/gopro/lib/convert"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
 )
 
 // 获取程序执行目录
@@ -11,4 +13,13 @@ func GetBasePath() string {
 	file, _ := exec.LookPath(os.Args[0])
 	path, _ := filepath.Abs(file)
 	return filepath.Dir(path)
+}
+
+/**
+ * 获取任意一天整点时间戳
+ */
+func GetDayTimestamp(day int) string {
+	d := time.Now().AddDate(0, 0, day).Format("20060102")
+	a, _ := time.ParseInLocation("20060102", d, time.Local)
+	return convert.ToString(a.Unix())
 }
--- 9xutool/model/usercoordinate.go
+++ 9xutool/model/usercoordinate.go
@@ -3,13 +3,14 @@ package model
 import (
 	"errors"
 	"fmt"
+	"github.com/ngaut/log"
+	"github.com/qgweb/gopro/lib/convert"
 	"github.com/qgweb/gopro/lib/orm"
 	"io/ioutil"
 	"runtime/debug"
 	"time"
 
 	"github.com/codegangsta/cli"
-	"github.com/ngaut/log"
 	"github.com/qgweb/gopro/9xutool/common"
 	"gopkg.in/ini.v1"
 	"gopkg.in/mgo.v2/bson"
@@ -20,7 +21,6 @@ type (
 		mp      *common.MgoPool
 		mysql   *orm.QGORM
 		iniFile *ini.File
-		// adFile	//经纬度和ad信息相关
 	}
 
 	TaoCat struct { //数据模型
@@ -29,14 +29,11 @@ type (
 		Cid   string
 		Pid   string
 	}
-
-//	Tags_num
 )
 
 var (
-	taocat_level_list map[int]map[string]*TaoCat //map[level]map[cid]TaoCat 用于取第三级标签信息
-	taocat_list       map[string]*TaoCat         //标签分类总表 map[cid]TaoCat
-	tags_num          = make(map[string]int)     //标签计数
+	taocat_list map[string]*TaoCat //标签分类总表 map[cid]TaoCat
+	tags_num    map[string]int     //标签计数
 )
 
 func NewUserCdCli() cli.Command {
@@ -71,20 +68,11 @@ func NewUserCdCli() cli.Command {
 			mconf.UserName = ur.iniFile.Section("mongo").Key("user").String()
 			mconf.UserPwd = ur.iniFile.Section("mongo").Key("pwd").String()
 			ur.mp = common.NewMgoPool(mconf)
-
 			//mysql 配置文件
-			myconf := &common.MysqlConfig{}
-			myconf.DBName = ur.iniFile.Section("mysql").Key("db").String()
-			myconf.Host = ur.iniFile.Section("mysql").Key("host").String()
-			myconf.Port = ur.iniFile.Section("mysql").Key("port").String()
-			myconf.UserName = ur.iniFile.Section("mysql").Key("user").String()
-			myconf.UserPwd = ur.iniFile.Section("mysql").Key("pwd").String()
-			ur.mysql = common.NewMysqlPool(myconf)
+			ur.mysql = orm.NewORM()
 
 			ur.initData()
 			ur.Do(c)
-			// ur.getTagsInfo(c)
-			fmt.Println(tags_num)
 		},
 	}
 }
@@ -100,7 +88,6 @@ func (this *UserCdTrace) initData() {
 		log.Error(err)
 	}
 
-	taocat_level_list = make(map[int]map[string]*TaoCat)
 	taocat_list = make(map[string]*TaoCat)
 	if len(list) > 0 {
 		for _, v := range list {
@@ -111,33 +98,70 @@ func (this *UserCdTrace) initData() {
 				Cid:   v["cid"].(string),
 				Pid:   v["pid"].(string),
 			}
-			if _, ok := taocat_level_list[category.Level]; !ok {
-				taocat_level_list[category.Level] = make(map[string]*TaoCat)
-			}
-			taocat_level_list[category.Level][category.Cid] = category
 			taocat_list[category.Cid] = category
 		}
 	}
+	log.Info("初始化taocat_list完毕")
 }
 
+//整体逻辑
+//从经纬度文件中提取出ad
+//for执行getTagsInfo方法，处理标签
+//最后把tags_num入库，入库的时候再做映射取标签中文名
 func (this *UserCdTrace) Do(c *cli.Context) {
-	//整体逻辑
-	//从经纬度文件中提取出ad
-	//for执行getTagsInfo方法，处理标签
-	//最后把tags_num入库，入库的时候再做映射取标签中文名
+	var (
+		db    = this.iniFile.Section("mongo").Key("db").String()
+		table = "jingweidu" //临时名字
+		sess  = this.mp.Get()
+	)
+	iter := sess.DB(db).C(table).Find(nil).Iter() //昨天的数据
+	i := 1
+	for {
+		log.Info("已处理", convert.ToString(i), "条记录")
+		i++
+		var info map[string]interface{}
+		if !iter.Next(&info) {
+			break
+		}
+		ad := info["ad"].(string)
+		this.getTagsInfo(ad)
+	}
+
+	if len(tags_num) > 0 {
+		this.getMysqlConnect() //连接mysql
+
+		DayTimestamp := common.GetDayTimestamp(-1)
+		for tag_id, num := range tags_num {
+			if _, ok := taocat_list[tag_id]; !ok {
+				continue
+			}
+			tag_text := taocat_list[tag_id].Name
+			this.mysql.BSQL().Insert("tags_report_day").Values("tag_id", "tag_text", "num", "time")
+			_, err := this.mysql.Insert(tag_id, tag_text, num, DayTimestamp)
+			if err != nil {
+				log.Warn("插入失败 ", err)
+			}
+		}
+	}
+	log.Info("数据分析完毕!")
 }
 
 func (this *UserCdTrace) getMysqlConnect() {
-	this.mysql = orm.NewORM()
-	err := this.mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",
-		user, pwd, host, port, db, charset))
-
+	var (
+		db      = this.iniFile.Section("mysql-jw").Key("db").String()
+		host    = this.iniFile.Section("mysql-jw").Key("host").String()
+		port    = this.iniFile.Section("mysql-jw").Key("port").String()
+		user    = this.iniFile.Section("mysql-jw").Key("user").String()
+		pwd     = this.iniFile.Section("mysql-jw").Key("pwd").String()
+		charset = "utf8"
+		err     = this.mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",
+			user, pwd, host, port, db, charset))
+	)
 	if err != nil {
-		log.Fatal("连接数据库失败：", err)
+		log.Fatal(err)
 	}
-
-	myorm.SetMaxIdleConns(50)
-	myorm.SetMaxOpenConns(100)
+	this.mysql.SetMaxIdleConns(50)
+	this.mysql.SetMaxOpenConns(100)
 }
 
 //根据ad获取标签
@@ -146,16 +170,18 @@ func (this *UserCdTrace) getTagsInfo(ad string) {
 		db       = this.iniFile.Section("mongo").Key("db").String()
 		sess     = this.mp.Get()
 		tagsInfo []map[string]interface{}
-		dayTime  = getDay(0)
+		dayTime  = getDay(-1) //0为今日
 	)
 	defer sess.Close()
 
+	_ = ad
 	err := sess.DB(db).C("useraction").Find(bson.M{"AD": ad, "day": dayTime}).All(&tagsInfo)
 	// err := sess.DB(db).C("useraction").Find(bson.M{"AD": "YwdLb0cZUVlABmVXcAhgeg==", "day": "20151206"}).All(&tagsInfo)
 	if err != nil {
 		log.Error(err)
 	}
 	if len(tagsInfo) > 0 {
+		tags_num = make(map[string]int)
 		for _, v := range tagsInfo { //可能会有多条数据，即多个ad
 			for _, tag := range v["tag"].([]interface{}) { //获取每个ad内的tagid
 				tagm := tag.(map[string]interface{})
@@ -187,7 +213,7 @@ func (this *TaoCat) getLv3Id() (string, error) {
 	}
 	tmp := *this
 	for i := 0; i < n; i++ {
-		tmp = *(taocat_list[tmp.Pid]) //获取父级
+		tmp = *taocat_list[tmp.Pid] //获取父级
 	}
 	return tmp.Cid, nil
 }
