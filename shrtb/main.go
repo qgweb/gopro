84e01cb2f2d20adb7ff05c8fc73a7638b59b078a
郑波 <zhengbo@pv25.com>
2016-04-06 16:36:09 +0800

    上海投放改造

--- shrtb/main.go
+++ shrtb/main.go
@@ -2,45 +2,40 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
+	"bufio"
 	"flag"
 	"fmt"
-	"github.com/qgweb/gopro/lib/convert"
+	"github.com/garyburd/redigo/redis"
 	"github.com/qgweb/gopro/lib/encrypt"
+	"github.com/qgweb/new/lib/config"
 	"io"
-	"io/ioutil"
 	"log"
-	"math/rand"
 	"net/http"
+	_ "net/http/pprof"
+	"net/url"
+	"os"
 	"runtime"
 	"strconv"
-	"sync"
+	"strings"
 	"time"
-
-	_ "net/http/pprof"
-
-	"bufio"
-	"github.com/garyburd/redigo/redis"
-	"gopkg.in/ini.v1"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
-	"os"
 )
 
 var (
-	conf       = flag.String("conf", "", "配置文件")
-	IniFile    *ini.File
-	err        error
-	pool       *redis.Pool
-	httpclient http.Client
-	mux        sync.Mutex
-	rtotal     uint64
-	stotal     uint64
-	adtotal    uint64
-	aduatotal  uint64
-	mdbsession *mgo.Session
-	fileChan   chan string
+	conf      = flag.String("conf", "", "配置文件")
+	IniFile   config.ConfigContainer
+	err       error
+	pool      *redis.Pool
+	aupool    *redis.Pool // 记录投放过的adua
+	rtotal    uint64
+	stotal    uint64
+	adtotal   uint64
+	aduatotal uint64
+	fileChan  chan string
+)
+
+const (
+	MODEL_REDIS = 1
+	MODEL_URL   = 2
 )
 
 func init() {
@@ -50,25 +45,22 @@ func init() {
 		log.Fatalln("配置文件不存在")
 	}
 
-	confData, err := ioutil.ReadFile(*conf)
+	IniFile, err = config.NewConfig("ini", *conf)
 	if err != nil {
 		log.Fatalln("读取配置文件出错,错误信息为:", err)
 	}
 
-	IniFile, err = ini.Load(confData)
-	if err != nil {
-		log.Fatalln("生成配置文件结构出错,错误信息为:", err)
-	}
-
-	pool = GetRedisPool()
-
+	pool = GetRedisPool(IniFile.String("redis::host"),
+		IniFile.String("redis::port"))
+	aupool = GetRedisPool(IniFile.String("auredis::host"),
+		IniFile.String("auredis::port"))
 	fileChan = make(chan string, 1000)
 }
 
 func main() {
 	var (
-		host = IniFile.Section("http").Key("host").String()
-		port = IniFile.Section("http").Key("port").String()
+		host = IniFile.String("http::host")
+		port = IniFile.String("http::port")
 	)
 
 	if host == "" || port == "" {
@@ -86,28 +78,9 @@ func main() {
 	log.Println(http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), nil))
 }
 
-func getRand(ary []int) int {
-	prosum := 0
-	for _, v := range ary {
-		prosum += v
-	}
-
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	for k, v := range ary {
-		randNum := r.Intn(prosum)
-		if randNum <= v {
-			return k
-		} else {
-			prosum -= v
-		}
-	}
-
-	return -1
-}
-
 //请求出价
 func requestPrice(w http.ResponseWriter, r *http.Request) {
+	var isopen = IniFile.String("default::open")
 	rtotal++
 	//http://$ip:$port/uri/?ad=$ad&ua=$ua&url=$url&mid=$mid&showType=01|02|03|04|05&price=$price
 	query := r.URL.Query()
@@ -131,7 +104,9 @@ func requestPrice(w http.ResponseWriter, r *http.Request) {
 		param[k] = v[0]
 	}
 
-	go reponsePrice(param)
+	if isopen == "1" {
+		go reponsePrice(param)
+	}
 }
 
 //请求出价
@@ -140,13 +115,11 @@ func reponsePrice(param map[string]string) {
 	//	defer mux.Unlock()
 
 	//	//http://ip:port/uri?mid=$mid&prod=$prod&showType=$showType
-	conn := pool.Get()
-	defer conn.Close()
-
 	var (
-		host  = IniFile.Section("dxhttp").Key("host").String()
-		port  = IniFile.Section("dxhttp").Key("port").String()
-		adurl = IniFile.Section("adurl").Key("url").String()
+		host  = IniFile.String("dxhttp::host")
+		port  = IniFile.String("dxhttp::port")
+		adurl = IniFile.String("dxhttp::url")
+		mode  = IniFile.String("default::mode")
 	)
 
 	if host == "" || port == "" {
@@ -161,136 +134,116 @@ func reponsePrice(param map[string]string) {
 		host, port, param["mid"], encrypt.GetEnDecoder(encrypt.TYPE_BASE64).Encode(adurl),
 		"03", "reBkYQmESMs=", "10")
 
+	if checkExistAd(param["ad"]) {
+		return
+	}
+
+	isput := false
+	switch mode {
+	case MODEL_REDIS:
+		isput = matchRedis(param) //redis匹配
+	case MODEL_URL:
+		isput = matchUrl(param) //url匹配
+	default: //所有
+		isput = matchRedis(param) || matchUrl(param)
+	}
+
+	if !isput {
+		return
+	}
+
+	resp, err := http.Get(url)
+	stotal++
+	if resp != nil {
+		resp.Body.Close()
+	}
+	if err != nil {
+		log.Println("发送数据出错,错误信息为:", err)
+		return
+	}
+	if resp != nil && resp.StatusCode != 200 {
+		log.Println("返回数据出错,错误code为:", resp.StatusCode)
+		return
+	}
+
+	recordPutAd(param["ad"])
+}
+
+// 匹配redis
+func matchRedis(param map[string]string) bool {
+	conn := pool.Get()
+	defer conn.Close()
+
 	conn.Do("SELECT", "1")
 
 	key := encrypt.DefaultMd5.Encode(param["ad"] + "_" + param["ua"])
 	r, err := redis.Bool(conn.Do("EXISTS", key))
 	if err != nil || !r {
-		return
+		return false
 		r, err = redis.Bool(conn.Do("EXISTS", param["ad"]))
 		if err != nil || !r {
-			return
+			return false
 		} else {
 			adtotal++
 		}
 	} else {
 		aduatotal++
 	}
-
-	resp, err := http.Get(url)
-
-	if resp != nil {
-		resp.Body.Close()
-	}
-
-	if err != nil {
-		log.Println("发送数据出错,错误信息为:", err)
-	}
-	if resp != nil && resp.StatusCode != 200 {
-		log.Println("返回数据出错,错误code为:", resp.StatusCode)
-	}
-
-	stotal++
+	return true
 }
 
-//获取关键字信息
-func getKeyWord(ad string) (string, error) {
-	defer func() {
-		if a := recover(); a != nil {
-			log.Println(a)
-		}
-	}()
-
-	tookenUrl := "http://61.129.39.71/telecom-dmp/getToken?apiKey=6c2d70c079160a60e9a91d7c514e3903&sign=dc1367fb302be81365de1dd20a1e4ffb3c1d81fd"
-	resp, err := http.Get(tookenUrl)
+// 解析url地址
+func parseUrl(ourl string) string {
+	if !(strings.Contains(ourl, "http://") || strings.Contains(ourl, "https://")) {
+		ourl = "//" + ourl
+	}
+	a, err := url.Parse(ourl)
 	if err != nil {
-		log.Println("获取token失败,错误信息:", err)
-		return "", err
+		return ""
 	}
-	defer resp.Body.Close()
-
-	tooken, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("读取tooken内容失败,错误信息:", err)
-		return "", err
-	}
-
-	var tookenJson map[string]interface{}
-
-	err = json.Unmarshal(tooken, &tookenJson)
-	if err != nil {
-		log.Println("解析json失败,错误信息:", err)
-		return "", err
-	}
-
-	tookenStr := ""
-
-	if convert.ToString(tookenJson["code"]) == "200200" &&
-		convert.ToString(tookenJson["message"]) == "OK" {
-		tookenStr = convert.ToString(tookenJson["result"])
-	} else {
-		log.Println("tooken获取失败")
-		return "", errors.New("tooken获取失败")
-	}
-
-	keyUrl := fmt.Sprintf("http://61.129.39.71/telecom-dmp/kv/getValueByKey?token=%s&table=qg_ds_kv&key=%s", tookenStr, ad)
-
-	resp, err = http.Get(keyUrl)
-	if err != nil {
-		log.Println("获取关键字信息失败,错误信息:", err)
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	key, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("读取关键字信息内容失败,错误信息:", err)
-		return "", err
-	}
-
-	var keyJson map[string]interface{}
-
-	err = json.Unmarshal(key, &keyJson)
-	if err != nil {
-		log.Println("解析json失败,错误信息:", err)
-		return "", err
-	}
-
-	if convert.ToString(keyJson["code"]) == "200200" &&
-		convert.ToString(keyJson["message"]) == "OK" &&
-		keyJson["result"] != nil {
-		return convert.ToString(keyJson["result"].(map[string]interface{})["value"]), nil
-	} else {
-		log.Println("关键字获取失败")
-		return "", errors.New("关键字获取失败")
-	}
+	return a.Host + a.Path
 }
 
-//记录信息
-func recordAdUa(param map[string]string) {
-	sess := GetSession()
-	defer sess.Close()
+// 匹配链接
+func matchUrl(param map[string]string) bool {
+	var db = IniFile.String("auredis::urldb")
+	var key = parseUrl(encrypt.DefaultBase64.Decode(param["url"]))
+	conn := aupool.Get()
+	defer conn.Close()
 
-	db := IniFile.Section("mongo").Key("db").String()
+	conn.Do("SELECT", db)
+	v, _ := redis.Bool(conn.Do("EXISTS", key))
+	return v
+}
 
-	res, err := getKeyWord(param["ad"])
-	if err != nil {
-		res = ""
+// 验证是否存在
+func checkExistAd(ad string) bool {
+	var db = IniFile.String("auredis::db")
+	var key = "SH_HPUT_" + time.Now().Format("20060102")
+	conn := aupool.Get()
+	defer conn.Close()
+
+	conn.Do("SELECT", db)
+
+	if r, _ := redis.Bool(conn.Do("HEXISTS", key, ad)); r {
+		return true
 	}
+	return false
+}
 
-	res = encrypt.GetEnDecoder(encrypt.TYPE_BASE64).Decode(res)
+// 记录投放过的ad
+func recordPutAd(ad string) {
+	var db = IniFile.String("auredis::db")
+	var key = "SH_HPUT_" + time.Now().Format("20060102")
+	conn := aupool.Get()
+	defer conn.Close()
 
-	sess.DB(db).C("srtb").Insert(bson.M{"ad": param["ad"], "ua": encrypt.GetEnDecoder(encrypt.TYPE_BASE64).Decode(param["ua"]),
-		"keys": res, "date": time.Now().Format("2006-01-02")})
+	conn.Do("SELECT", db)
+	conn.Do("HSET", key, ad, 1)
 }
 
 //redis链接此
-func GetRedisPool() *redis.Pool {
-	var (
-		host = IniFile.Section("redis").Key("host").String()
-		port = IniFile.Section("redis").Key("port").String()
-	)
-
+func GetRedisPool(host, port string) *redis.Pool {
 	return &redis.Pool{
 		MaxIdle:   100,
 		MaxActive: 100, // max number of connections
@@ -302,31 +255,6 @@ func GetRedisPool() *redis.Pool {
 			return c, err
 		},
 	}
-}
-
-//获取mongo-session
-func GetSession() *mgo.Session {
-	mux.Lock()
-	defer mux.Unlock()
-
-	if mdbsession == nil {
-		var (
-			err    error
-			mouser = IniFile.Section("mongo").Key("user").String()
-			mopwd  = IniFile.Section("mongo").Key("pwd").String()
-			mohost = IniFile.Section("mongo").Key("host").String()
-			moport = IniFile.Section("mongo").Key("port").String()
-			modb   = IniFile.Section("mongo").Key("db").String()
-		)
-		fmt.Println(fmt.Sprintf("%s:%s@%s:%s/%s", mouser, mopwd, mohost, moport, modb))
-		mdbsession, err = mgo.Dial(fmt.Sprintf("%s:%s@%s:%s/%s", mouser, mopwd, mohost, moport, modb))
-		if err != nil {
-			panic(err)
-		}
-	}
-	//高并发下会关闭连接,ping下会恢复
-	mdbsession.Ping()
-	return mdbsession.Copy()
 }
 
 func recordBiddingRuquest() {
